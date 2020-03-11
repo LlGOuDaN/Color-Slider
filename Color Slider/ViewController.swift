ff836cccaca80528ea5521491deb987540cd3db4
Yifei Li <liy12@rose-hulman.edu>
2020-03-11 08:33:02 -0400

    Initial Commit

---
+++ Color Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Slider
+//
+//  Created by Yifei Li on 3/11/20.
+//  Copyright © 2020 Yifei Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
